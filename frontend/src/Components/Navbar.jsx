b04485dfd2673564c1191ece94a32f8fa492974d
Kiirankale <kirankale431@gmail.com>
2025-02-11 19:51:13 +0530

    Enhance Navbar with user dropdown and logout functionality

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,17 +1,64 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../App.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { CartModal } from '../Pages/Shop/ProductDetails/CartModal';
+import avatarImg from '../assets/avatar.png'
+import { useLogoutUserMutation } from '../redux/Features/auth/authApi';
+import { logout } from '../redux/Features/auth/authSlice';
+
 
 function Navbar() {
 
-    const products =useSelector((state)=>state.cart.products)
+    const products = useSelector((state) => state.cart.products)
 
-    const [isCartOpen,setIsCartOpen]=useState(false);
-    const handleCartToggle =()=>{
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const handleCartToggle = () => {
         setIsCartOpen(!isCartOpen)
     }
+
+    // show user if logged in
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth);
+    const [logoutUser]= useLogoutUserMutation()
+    const navigate = useNavigate()
+    
+
+    // dropdown menu
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+    const handleDropDownToggle = () => {
+        setIsDropDownOpen(!isDropDownOpen)
+
+    }
+    // admin dropdown menus
+    const adminDropDownMenus = [
+        { label: "Dashboard", path: '/dashboard/admin' },
+        { label: "Manage Items", path: '/dashboard/manage-products' },
+        { label: "All Orders", path: '/dashboard/manage-orders' },
+        { label: "Add New Post", path: '/dashboard/add-new-post' }
+    ]
+    // userDropDown menus 
+    const userDropDownMenus = [
+        { label: "Dashboard", path: '/dashboard' },
+        { label: "Profile", path: '/dashboard/manage-products' },
+        { label: "Payments", path: '/dashboard/manage-orders' },
+        { label: "Orders", path: '/dashboard/add-new-post' }
+
+    ]
+
+    const dropDownMenus = user?.role === "admin" ? [...adminDropDownMenus] : [...userDropDownMenus]
+
+    const handleLogout = async () => {
+        try {
+            const response = await logoutUser().unwrap(); 
+            
+            dispatch(logout())
+            navigate('/login');
+        } catch (error) {
+            console.error("Failed to logout user:",error);
+        }
+    };
+
     return (
         <header className='fixed-nav-bar w-nav'>
             <nav className='max-w-screen-2xl mx-auto px-4 flex justify-between items-center '>
@@ -41,9 +88,26 @@ function Navbar() {
                         </button>
                     </span>
                     <span>
-                        <Link to={"/user"}>
+                        {user ? (<>
+                            <img onClick={handleDropDownToggle} src={user?.profileImg || avatarImg} alt="" className='size-6 cursor-pointer rounded-full' />
+                            {isDropDownOpen && (<div className='absolute mt-3 p-4 w-48 right-0 z-50 bg-white rounded-lg shadow-lg border border-gray-200' >
+                                <ul className='font-medium p-2 space-y-4'>
+                                    {
+                                        dropDownMenus.map((menu, index) => (
+                                            <li key={index} >
+                                                <Link onClick={()=>setIsDropDownOpen(false)}  className='dropdown-items' to={menu.path}>{menu.label}</Link>
+                                            </li>
+                                        ))
+                                    }
+                                    <li><Link onClick={handleLogout} className='dropdown-items'>Logout</Link></li>
+                                </ul>
+
+                            </div>)}
+                        </>
+                        ) : (<Link to='login'>
                             <i className="ri-user-smile-line"></i>
-                        </Link>
+                        </Link>)}
+
                     </span>
 
                 </div>
