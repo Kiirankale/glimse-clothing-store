aa6e9062e89a40b2e42a78db8959ca4e2538af8d
Kiirankale <kirankale431@gmail.com>
2025-03-04 07:08:57 +0530

    Update verifyToken.js

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -1,30 +1,27 @@
-const jwt = require('jsonwebtoken')
+
+const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
     try {
         const token = req.cookies.token;
-        console.log(token ,"line")
+        // const token = req.headers["authorization"].split(" ")[1]
        
-        if (!token) {
-            return res.status(401).send({ message: "Invalid token ." })
+        if(!token) {
+            return res.status(401).send({message: 'Invalid token'})
         }
         const decoded = jwt.verify(token, JWT_SECRET);
         if(!decoded){
-            return res.status(401).send({message:"Invalid token or not valid "})
+            return res.status(401).send({message: 'Invalid token or not valid'})
         }
         req.userId = decoded.userId;
-        req.role = decoded.role
+        req.role = decoded.role;
         next();
-
-
     } catch (error) {
-        console.error("Token verification failed:", error);
-        return res.status(401).json({ message: "Unauthorized access. Invalid token." });
-
+        console.error('Error while verifying token', error);
+        res.status(401).send({message: 'Error while verifying token'})
     }
-
 }
 
 module.exports = verifyToken;
