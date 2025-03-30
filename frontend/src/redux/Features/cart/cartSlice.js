6ed5819b0f03e264b946a1d74825014909648567
Kiirankale <kirankale431@gmail.com>
2025-03-30 11:12:09 +0530

    Update cartSlice.js

--- frontend/src/redux/Features/cart/cartSlice.js
+++ frontend/src/redux/Features/cart/cartSlice.js
@@ -17,45 +17,39 @@ const cartSlice = createSlice({
     reducers: {
         addToCart: (state, action) => {
             console.log("Payload:", action.payload);
-            const isExist = state.products.find((product) => product.id === action.payload.id);
+            const isExist = state.products.find((product) => product._id === action.payload._id);
             if (!isExist) {
-                state.products.push({ ...action.payload, quantity: 1 })
-
+                state.products.push({ ...action.payload, quantity: 1 });
             } else {
-                console.log('Items already added')
-            };
-
+                isExist.quantity += 1;  // 
+            }
+        
             state.selectedItems = setSelectedItems(state);
             state.totalPrice = setTotalPrice(state);
             state.tax = setTax(state);
-            state.grandTotal = setGrandTotal(state)
-
+            state.grandTotal = setGrandTotal(state);
         },
         updateQuantity: (state, action) => {
-            const products = state.products.map((product) => {
-                if (product.id === action.payload.id) {
+            state.products = state.products.map((product) => { 
+                if (product._id === action.payload._id) {
                     if (action.payload.type === "increment") {
-                        product.quantity += 1;
-
-                    } else if (action.payload.type === "decrement") {
-                        if (product.quantity > 1) {
-                            product.quantity -= 1;
-                        }
-                        return product;
+                        return { ...product, quantity: product.quantity + 1 };  
+                    } else if (action.payload.type === "decrement" && product.quantity > 1) {
+                        return { ...product, quantity: product.quantity - 1 };  
                     }
-
-
                 }
+                return product; 
             });
+        
+           
             state.selectedItems = setSelectedItems(state);
             state.totalPrice = setTotalPrice(state);
             state.tax = setTax(state);
-            state.grandTotal = setGrandTotal(state)
-
+            state.grandTotal = setGrandTotal(state);
         },
         removeFromCart: (state, action) => {
             state.products = state.products.filter((product) => {
-                return product.id !== action.payload.id;
+                return product._id !== action.payload._id;
             })
             state.selectedItems = setSelectedItems(state);
             state.totalPrice = setTotalPrice(state);
